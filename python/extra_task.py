bb7fec1144a67ed9a93260fff15ce88226b1329e
Ariel-ksenzovsky <a.ksenzovsky@gmail.com>
2024-11-19 10:05:27 +0200

    upgrade task

--- python/extra_task.py
+++ python/extra_task.py
@@ -1,7 +1,8 @@
 import csv
 from datetime import datetime
-
-today = datetime.now().strftime("%d.%m")
+today = datetime.now().strftime("%d.%m.%Y")
+today_day = datetime.now().strftime("%d")
+today_month = datetime.now().strftime("%m")
 today_year = datetime.now().strftime("%Y")
 
 print(f"today in {today}, Checks if someone has a birthday...")
@@ -13,18 +14,21 @@ def class_names():
         for line in csv_file:
             parts = line.strip().split(",")
             full_name = parts[0] 
-            birth_date = parts[-1].strip()
+            birth_date = parts[-1].strip().split(".")
+            birth_day = birth_date[0]
+            birth_month = birth_date[1]
+            birth_year = birth_date[2]
 
-            if len(birth_date) != 10 or birth_date[2] != '.' or birth_date[5] != '.':
-                print(f"Skipping invalid birth date: {birth_date}")
-                continue
+            # if len(birth_date) != 10 or birth_date[2] != '.' or birth_date[5] != '.':
+            #     print(f"Skipping invalid birth date: {birth_date}")
+            #     continue
         
-            birth_day_month = birth_date[:5]
-            birth_year = birth_date[-4:]
+            # birth_day_month = birth_date[:5]
+            # birth_year = birth_date[-4:]
             age = int(today_year) - int(birth_year) 
     
        
-            if birth_day_month == today:
+            if int(birth_day) == int(today_day) and int(birth_month) == int(today_month):
                 print(f"Heppy birthday, {full_name} for your {age} birthday ")
                 birthday_found = True
                 
